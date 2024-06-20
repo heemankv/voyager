1985e6436c304f7140c160a830222ffe80cc9754
Heemank Verma <heemankv@gmail.com>
2024-06-20 21:35:42 +0530

    update: Inital Setup for Frontend

---
+++ frontend/src/app/tx/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function IndividualTxnPage() {
+  return (
+    <div>IndividualTxnPage</div>
+  )
+}
---
+++ frontend/src/app/txns/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TransactionsList() {
+  return (
+    <div>TransactionsList</div>
+  )
+}
